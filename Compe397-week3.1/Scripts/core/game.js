5eaa1bb18b68aabc9062db693f6a237d7f048a76
Avneet <avneet_KochharEmail>
2015-09-25 18:01:26 -0400

    added

--- Compe397-week3.1/Scripts/core/game.js
+++ Compe397-week3.1/Scripts/core/game.js
@@ -1,42 +1,27 @@
-﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/stats/stats.d.ts" />
 /// <reference path="../typings/createjs-lib/createjs-lib.d.ts" />
 /// <reference path="../typings/easeljs/easeljs.d.ts" />
 /// <reference path="../typings/tweenjs/tweenjs.d.ts" />
 /// <reference path="../typings/soundjs/soundjs.d.ts" />
 /// <reference path="../typings/preloadjs/preloadjs.d.ts" />
-
-
-
-//global game framework variablesccc
-var canvas: HTMLElement;
-var stage: createjs.Stage;
-var stats: Stats;
-
-var helloLAbel: createjs.Text;
-
-function init():void {
+//global game framework variables
+var canvas;
+var stage;
+var stats;
+var helloLAbel;
+function init() {
     console.log("gmae started....");
     canvas = document.getElementById("canvas");
-    stage = new createjs.Stage(canvas);//passing canvas to stage
+    stage = new createjs.Stage(canvas); //passing canvas to stage
     createjs.Ticker.setFPS(60); //set the frame rate to 60 fps
-    createjs.Ticker.on("tick", gameloop);//update gameloop every frame
-
-    main();//call main function method
-
-
-} 
-
-function gameloop(event: createjs.Event): void {
-    stage.update();
-
-
+    createjs.Ticker.on("tick", gameloop); //update gameloop every frame
+    main(); //call main function method
 }
-
-
-
-
-function main(): void {
+function gameloop(event) {
+    stage.update();
+}
+function main() {
     helloLAbel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
     helloLAbel.regX = helloLAbel.getBounds().width * 0.5;
     helloLAbel.regY = helloLAbel.getBounds().height * 0.5;
@@ -44,5 +29,4 @@ function main(): void {
     helloLAbel.y = 240;
     stage.addChild(helloLAbel);
 }
-
-
+//# sourceMappingURL=game.js.map
